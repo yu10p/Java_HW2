4131037852eae4805f57866a150645a748c4aeb1
yu10p <eason31003@gmail.com>
2022-10-22 19:17:46 +0800

    Java_HW2 version_1

---
+++ p08.java
@@ -0,0 +1,24 @@
+package hw2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p08 {
+	public static void main(String[] args)throws IOException {
+		System.out.println("請輸入整數");
+		
+		BufferedReader br =new BufferedReader(
+				new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		//if 沒有大括號的情況
+		if(num == 1)
+		
+			System.out.println("輸入的是1");
+			System.out.println("選擇的是1");
+		
+		
+		System.out.println("結束處理");
+	}
+}
---
+++ p26.java
@@ -0,0 +1,14 @@
+package hw2;
+
+public class p26 {
+	public static void main(String[] args) {
+		int i = 1;
+		
+		while(i <= 5) {
+			System.out.println("第"+ i + "次的迴圈");
+			i++;
+		}
+		
+		System.out.println("迴圈結束");
+	}
+}
